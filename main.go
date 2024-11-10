7e8364f0d0acf1f5b55d6f7ae6e53ce48467e394
imago <l.kinde94@outlook.com>
2024-11-10 09:46:53 +0200

    Added docs to munchausen process

--- main.go
+++ main.go
@@ -7,14 +7,17 @@ import (
 	"time"
 )
 
+// main function containing _everything_ since that just made it easier
 func main() {
 	t0 := time.Now()
+	// First parse out the environment variables to determine when
 	healthyAfterStr := os.Getenv("HEALTHY_AFTER_DURATION")
 	if healthyAfterStr == "" {
 		panic("Missing environment variable 'HEALTHY_AFTER_DURATION'")
 	}
 	healthyAfter, err := time.ParseDuration(healthyAfterStr)
 
+	// Lookup if we should feign unhealthiness at all
 	unhealthyAfterStr, shouldGetUnhealthy := os.LookupEnv("UNHEALTHY_AFTER_DURATION")
 	var unhealthyAfter time.Duration
 	if shouldGetUnhealthy {
@@ -28,27 +31,33 @@ func main() {
 		panic(fmt.Sprintf("failed to parse 'UNHEALTHY_AFTER_DURATION': %v", err))
 	}
 
+	// Setup the handler here, reducing the need for complexity
 	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		// It works by checking the time since start on every request
 		timeSinceStart := time.Since(t0)
+		// If we've passed the time for turning healthy, do logic (see below)
+		// On the other hand, if we haven't yet reached 'healthy', don't return anything
+		// at all to simulate a booting service
 		if timeSinceStart > healthyAfter {
+			// ..check if we instead should be unhealthy
 			if shouldGetUnhealthy && timeSinceStart > unhealthyAfter {
 				unhealthySince := timeSinceStart - unhealthyAfter
+				// Note that this is a 'kind' way of being unhealthy
+				// TODO: Deadlock/performance unhealthiness simulations, where response times out
 				w.WriteHeader(http.StatusInternalServerError)
 				fmt.Fprintf(w, "Time's up, now I'm unhealthy! Been unhealthy for: %v", unhealthySince)
 				return
 			}
+			// otherwise report healthy
 			out := "Now I'm healthy."
 			if shouldGetUnhealthy {
 				out += fmt.Sprintf(" Turning unhealthy in: %v", unhealthyAfter-timeSinceStart)
 			}
 			w.WriteHeader(http.StatusOK)
 			w.Write([]byte(out))
-		} else {
-			bootupDoneAt := healthyAfter - timeSinceStart
-			w.WriteHeader(http.StatusInternalServerError)
-			fmt.Fprintf(w, "Not ready and therefore not healthy. 'Bootup' done in: %v", bootupDoneAt)
 		}
 	})
+
 	fmt.Println("serving health status on localhost:8080/health")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		panic(fmt.Sprintf("failed to serve: %v", err))
